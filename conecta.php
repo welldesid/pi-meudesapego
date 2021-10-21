8bf45b17c6207bbe1d1472108401f9ccc0fc6b39
Débora Thomaz <deh.thomaz@gmail.com>
2021-10-21 20:30:09 -0300

    Teste 2 gitignore

--- conecta.php
+++ conecta.php
@@ -1,5 +1,5 @@
 <?php  
 	$conexao = mysqli_connect("127.0.0.1", "root", "", "doacoesong", 3308); //endereço do servidor (pode ser "localhost"), usuário, senha, banco de dados, e porta do host
-	//teste gitignore
+	//teste gitignore 2
 	
 ?>
