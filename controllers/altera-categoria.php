596ba095822b33984146d02ec98142ecab0c1ab3
dehbyTR <deh.thomaz@gmail.com>
2021-11-05 14:59:20 -0300

    Controller para alterar categoria

--- controllers/altera-categoria.php
+++ controllers/altera-categoria.php
@@ -8,4 +8,24 @@
 	//Fim do que precisa estar no cabeçalho
 	
 	//verificaUsuario();
+	
+	$nome = $_POST["nome"];
+
+	$categoria = new Categoria($nome);
+	$categoria->setId($_POST["idcategoria"]);
+
+	$categoriaDao = new CategoriaDao($conexao);
+
+	if ($categoriaDao->alteraCategoria($categoria)) {
+?>
+		<p class="alert-success">Categoria alterada com sucesso!</p>
+<?php
+	} else {
+		$msg = mysqli_error($conexao); //me devolve uma mensagem de erro
+?>
+		<p class="alert-danger">Oh-Oh! Algo deu errado! =/</p>
+		<p>Erro: <?= $msg ?> </p>
+<?php
+	}
+	
 ?>
