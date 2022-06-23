d0b575209b119fa68b729ce527755b0c5fe67f9a
ViktorOliveira140 <victoroliver140@gmail.com>
2022-06-23 00:26:03 -0300

    Correções
    Correção de sintaxe do formulário

--- cad_doadorv2.php
+++ cad_doadorv2.php
@@ -22,68 +22,68 @@
         <h2 class="text-center">Registrar-se</h2>   
         <div class="form-group">
             <label for="nomecompleto">Nome Completo</label>
-            <input type="text" class="form-control" id="nomecompleto" aria-describedby="emailAjuda">
+            <input type="text" name="nomecompleto" class="form-control" id="nomecompleto" aria-describedby="nomeAjuda">
             <small id="nomeAjuda" class="form-text text-muted">Exemplo: José da Silva</small>
         </div>  
 
         <div class="form-group">
             <label for="datanascimento">Data de Nascimento</label>
-            <input type="date" class="form-control" id="datanascimento" aria-describedby="datanascimentoAjuda">
+            <input type="date" name="datanasc" class="form-control" id="datanasc" aria-describedby="datanascimentoAjuda">
             <small id="nomeAjuda" class="form-text text-muted">Selecione uma data válida</small>
         </div> 
 
         <div class="form-group">
             <label for="cep">CEP</label>
-            <input type="text" class="form-control" id="cep" aria-describedby="cepAjuda">
+            <input type="text" name="cep" class="form-control" id="cep" aria-describedby="cepAjuda">
             <small id="cepAjuda" class="form-text text-muted">Exemplo: 13222621</small>
         </div> 
 
 
         <div class="form-group">
             <label for="rua">Rua</label>
-            <input type="text" class="form-control" id="rua" aria-describedby="ruaAjuda">
+            <input type="text" name="rua" class="form-control" id="rua" aria-describedby="ruaAjuda">
             <small id="ruaAjuda" class="form-text text-muted">Exemplo: Avenida das Rosas</small>
         </div> 
 
         <div class="form-group">
             <label for="numero">Número</label>
-            <input type="text" class="form-control" id="numero" aria-describedby="numeroAjuda">
+            <input type="text" name="numero" class="form-control" id="numero" aria-describedby="numeroAjuda">
             <small id="numeroAjuda" class="form-text text-muted">Exemplo: 339</small>
         </div>
 
         <div class="form-group">
             <label for="complemento">Complemento</label>
-            <input type="text" class="form-control" id="complemento" aria-describedby="complementoAjuda">
+            <input type="text" name="complemento" class="form-control" id="complemento" aria-describedby="complementoAjuda">
             <small id="complementoAjuda" class="form-text text-muted">Exemplo: Residencial Tulipas</small>
         </div> 
 
         <div class="form-group">
             <label for="estado">Estado</label>
-            <input type="text" class="form-control" id="estado" aria-describedby="estadoAjuda">
+            <input type="text" name="estado" class="form-control" id="estado" aria-describedby="estadoAjuda">
             <small id="estadoAjuda" class="form-text text-muted">Exemplo: SP</small>
         </div> 
 
         <div class="form-group">
             <label for="cidade">Cidade</label>
-            <input type="text" class="form-control" id="cidade" aria-describedby="cidadeAjuda">
+            <input type="text" name="cidade" class="form-control" id="cidade" aria-describedby="cidadeAjuda">
             <small id="cidadeAjuda" class="form-text text-muted">Exemplo: Itatiba</small>
         </div> 
 
         <div class="form-group">
             <label for="bairro">Bairro</label>
-            <input type="text" class="form-control" id="bairro" aria-describedby="bairroAjuda">
+            <input type="text" name="bairro" class="form-control" id="bairro" aria-describedby="bairroAjuda">
             <small id="bairroAjuda" class="form-text text-muted">Exemplo: Jardim Tamboré</small>
         </div> 
 
         <div class="form-group">
             <label for="cidade">Senha</label>
-            <input type="password" class="form-control" id="senha" aria-describedby="senhaAjuda">
+            <input type="password" name="senha" class="form-control" id="senha" aria-describedby="senhaAjuda">
             <small id="senhaAjuda" class="form-text text-muted">Digite uma senha válida para autenticação</small>
         </div> 
 
 
         <div class="clearfix">
-            <label class="float-left form-check-label"><input type="checkbox"> Aceito os <a href="SELECIONARARQUIVODETERMOS">Termos de Serviço </a></label> 
+            <label class="float-left form-check-label"><input id="checktermos" required type="checkbox"> Aceito os <a href="SELECIONARARQUIVODETERMOS">Termos de Serviço </a></label> 
         </div><p> 
 
 
